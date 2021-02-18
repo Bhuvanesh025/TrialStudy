3743b1a1e97aa6f08df1d99879175dc9e3f426e8
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-02-18 17:17:08 +0530

    BranchB has added his code

---
+++ src/org/add/FamilyMembers.java
@@ -0,0 +1,37 @@
+package org.add;
+
+
+public class FamilyMembers {
+	public void family1() {
+		System.out.println("Family-Grand mother");
+	}
+	public void family2() {
+		System.out.println("Family-Mother");
+	}
+	public void family3() {
+		System.out.println("Family-Brother");
+	}
+	public void family4() {
+		System.out.println("Family-Sister");
+	}
+	public static void main(String[] args) {
+		FamilyMembers a = new FamilyMembers();
+		FamilyMembers b = new FamilyMembers();
+		FamilyMembers c = new FamilyMembers();
+		FamilyMembers d = new FamilyMembers();
+		a.family1();
+		b.family2();
+		c.family3();
+		d.family4();
+		
+
+}
+}
+
+
+
+
+
+
+
+
