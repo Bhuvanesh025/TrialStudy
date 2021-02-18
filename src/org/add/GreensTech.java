08fb5b3f250cae7f2b45a04b569fb4b9e2254147
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-02-18 16:59:16 +0530

    BranchA added his code

--- src/org/add/GreensTech.java
+++ src/org/add/GreensTech.java
@@ -7,11 +7,18 @@ public void greensOmr() {
 public void greensAdayar() {
 	System.out.println("Greens Technologies,Adayar,Chennai");
 }
+public void greensErode() {
+	System.out.println("Greens Technologies,Erode");
+}
+public void greensCoimbatore() {
+	System.out.println("Greens Technologies,Coimbatore");
+}
 public static void main(String[] args) {
 	GreensTech b =new GreensTech();
 	b.greensOmr();
 	b.greensAdayar();
-	
+	b.greensErode();
+	b.greensCoimbatore();
 }
 }
 
