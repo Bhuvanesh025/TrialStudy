458c6ac09c90e0ca1eaaff880f6de739a8f76247
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-02-18 14:31:49 +0530

    A as added his code

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class GreensTech {
+public void greensOmr() {
+	System.out.println("Greens Technologies,OMR,Chennai");
+}
+public void greensAdayar() {
+	System.out.println("Greens Technologies,Adayar,Chennai");
+}
+public void greensTambaram() {
+	System.out.println("Greens Technologies,Tambaram,Chennai");
+}
+public void greensVelacherry() {
+	System.out.println("Greens Technologies,Velacherry,Chennai");
+}
+public void greensAnnaNagar() {
+	System.out.println("Greens Technologies,AnnaNagar,Chennai");
+}
+public static void main(String[] args) {
+	GreensTech b =new GreensTech();
+	b.greensOmr();
+	b.greensAdayar();
+	b.greensTambaram();
+	b.greensVelacherry();
+	b.greensAnnaNagar();
+}
+}
+
+
+
+
+
+
+
+
